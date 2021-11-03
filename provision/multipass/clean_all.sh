134c0703a75d6e905555eb1f3ed563e29c6983b6
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-03 23:02:46 +0530

    ansible requirements

--- provision/multipass/clean_all.sh
+++ provision/multipass/clean_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+multipass ls | grep -v Name | awk '{print $1}' | xargs multipass stop
 multipass ls | grep -v Name | awk '{print $1}' | xargs multipass delete
 multipass purge
 rm -fr config/cloud-init.yaml config/createusers.yal inventory

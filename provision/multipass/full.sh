b8481cd507824e4d816578c74979796f8151f5f4
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-04 17:00:38 +0530

    ansible requirements

--- provision/multipass/full.sh
+++ provision/multipass/full.sh
@@ -12,7 +12,6 @@ start=$(date +%s)
 export VM_NAME=control-center && provision/multipass/setup.sh
 export VM_NAME=mmonit && provision/multipass/setup.sh
 export VM_NAME=observability && provision/multipass/setup.sh
-export VM_NAME=dashboard && provision/multipass/setup.sh
 export VM_NAME=reverse-proxy && provision/multipass/setup.sh
 end=$(date +%s)
 runtime=$((end-start))

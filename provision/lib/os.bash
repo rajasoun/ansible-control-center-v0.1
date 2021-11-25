35f0c719526159904d35517ea90ed17a4e1036b3
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-26 01:04:10 +0530

    feat(tooling): pre-commit support

--- provision/lib/os.bash
+++ provision/lib/os.bash
@@ -83,3 +83,28 @@ function create_config_from_template() {
 
     echo "$USER_CONFIG_FILE & $CLOUD_INIT_CONFIG_FILE Generated"
 }
+
+# Workaround for Path Limitations in Windows
+function _docker() {
+  export MSYS_NO_PATHCONV=1
+  export MSYS2_ARG_CONV_EXCL='*'
+
+  case "$OSTYPE" in
+      *msys*|*cygwin*) os="$(uname -o)" ;;
+      *) os="$(uname)";;
+  esac
+
+  if [[ "$os" == "Msys" ]] || [[ "$os" == "Cygwin" ]]; then
+      # shellcheck disable=SC2230
+      realdocker="$(which -a docker | grep -v "$(readlink -f "$0")" | head -1)"
+      printf "%s\0" "$@" > /tmp/args.txt
+      # --tty or -t requires winpty
+      if grep -ZE '^--tty|^-[^-].*t|^-t.*' /tmp/args.txt; then
+          #exec winpty /bin/bash -c "xargs -0a /tmp/args.txt '$realdocker'"
+          winpty /bin/bash -c "xargs -0a /tmp/args.txt '$realdocker'"
+          return 0
+      fi
+  fi
+  docker "$@"
+  return 0
+}

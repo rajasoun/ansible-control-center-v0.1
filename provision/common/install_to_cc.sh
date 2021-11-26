1b01a7d5a2bece92b43c73247d1c691b4db74b0b
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-26 09:48:37 +0530

    feat(tooling): pre-commit support

--- provision/common/install_to_cc.sh
+++ provision/common/install_to_cc.sh
@@ -8,17 +8,18 @@ SCRIPT_DIR="$(git rev-parse --show-toplevel)"
 source "$SCRIPT_DIR/provision/lib/install.bash"
 
 VM_NAME=${VM_NAME:-"control-center"}
-VM_HOME=${VM_HOME:-"/home/ubuntu"}
+ANSIBLE_HOME=${ANSIBLE_HOME:-"/home/ubuntu/ansible-control-center"}
 
 
 install_ansible
 install_ansible_roles
 
+if [ ! -f "inventory" ]; then
+    echo "Inventory File Not Availabe. Exiting..."
+    exit 1
+fi
+
 ansible-playbook playbooks/control_center.yml
 ansible-playbook playbooks/pip-packages.yml
 
-if [ ! -f "inventory" ]; then
-    echo "Inventory File Not Availabe. Exiting..."
-    return 0
-fi
 ansible -m ping all
--- provision/lib/install.bash
+++ provision/lib/install.bash
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 function install_ansible(){
-    sudo apt update
-    sudo apt install software-properties-common
-    sudo add-apt-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible -y
+    if grep -q docker /proc/1/cgroup; then
+        echo "Inside Docker Ansible Container. Skipping ansible upgrade"
+        export ANSIBLE_HOME="/ansible"
+    else
+        echo "On VM. Upgrading Ansible"
+            sudo apt update
+            sudo apt install software-properties-common
+            sudo add-apt-repository --yes --update ppa:ansible/ansible
+            sudo apt install ansible -y
+    fi
 }
 
 function install_ansible_roles(){
-    ansible-galaxy install -r $VM_HOME/ansible-control-center/dependencies/monitoring/requirements.yml
+    ansible-galaxy install -r $ANSIBLE_HOME/dependencies/monitoring/requirements.yml
     IP=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
 
     case "$IP" in
@@ -16,7 +22,7 @@ function install_ansible_roles(){
             echo "Multipass VM.Skipping User Mgmt";;
 
         * ) echo "User Mgmt"
-            ansible-galaxy install -r $VM_HOME/ansible-control-center/dependencies/user-mgmt/requirements.yml
+            ansible-galaxy install -r $ANSIBLE_HOME/dependencies/user-mgmt/requirements.yml
 
             local USER_TEMPLATE_FILE="config/templates/createusers.yml"
             local USER_CREATE_FILE="playbooks/createusers.yml"
@@ -27,7 +33,8 @@ function install_ansible_roles(){
             fi
             echo "Generating Config Files..."
             cp "$USER_TEMPLATE_FILE" "$USER_CREATE_FILE"
-
+            local SSH_KEY_PATH="keys"
+            local SSH_KEY="id_rsa"
             file_replace_text "_SSH_KEY_.*$" "$(cat "$SSH_KEY_PATH"/"${SSH_KEY}".pub)" "$USER_CONFIG_FILE"
             echo "$USER_CREATE_FILE  Generated"
 

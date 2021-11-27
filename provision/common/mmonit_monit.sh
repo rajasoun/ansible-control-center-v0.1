dc1e92c0c758d1c68e6d1e7f2f107d329b8c1b41
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-27 15:24:37 +0530

    feat(tooling): pre-commit support

--- provision/common/mmonit_monit.sh
+++ provision/common/mmonit_monit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+PLAYBOOK_HOME=$HOME/ansible-control-center
 ansible-galaxy install -r dependencies/monitoring/requirements.yml
 
 export MMONIT_LICENSE="$HOME/.ansible/roles/rajasoun.ansible_role_mmonit/files/license.yml"
@@ -7,11 +8,11 @@ ansible-vault decrypt $MMONIT_LICENSE --vault-password-file $HOME/ansible-manage
 echo "MMonit License Decrypt Done"
 
 # Install & Configure MMonit
-ansible-playbook  playbooks/mmonit.yml
+ansible-playbook  $PLAYBOOK_HOME/playbooks/mmonit.yml
 echo "${GREEN}MMonit Installation & Configuration Done!${NC}"
 
 # Install & Configure Monit
-ansible-playbook playbooks/monit.yml
+ansible-playbook $PLAYBOOK_HOME/playbooks/monit.yml
 echo "${GREEN}Monit Installation & Configuration Done!${NC}"
 
 ansible-vault encrypt $MMONIT_LICENSE --vault-password-file $HOME/ansible-managed/.vault_password
